fa308afed5650c0d94e7c619bcc11850b0e066e0
Genevieve Schultz <genschultz@gmail.com>
2018-08-09 11:09:31 -0700

    added style

--- gallery/static/js/app.js
+++ gallery/static/js/app.js
@@ -1,3 +1,8 @@
 $('document').ready(function() {
-
+  console.log("jQuery loaded");
+  var url = window.location.href;
+  var pathname = window.location.pathname;
+  if (url === 'http://localhost:8000/login/') {
+    $('#login').addClass()
+  }
 });
