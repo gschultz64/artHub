37812e60b076eba2440503aabbe5a09bda042c25
Genevieve Schultz <genschultz@gmail.com>
2018-08-14 09:00:12 -0700

    copied static files

---
+++ staticfiles/js/app.js
@@ -0,0 +1,35 @@
+// Change Active Class on Navigation Links
+var pathname = window.location.pathname;
+if (pathname !== '/') {
+  $("#home").removeClass("active");
+  if (pathname === '/login/') {
+    $('#login').addClass('active');
+  } else if (window.location.toString().includes('/user/')
+    && window.location.toString().includes('/upload/')) {
+    $('#upload').addClass('active');
+  } else if (pathname === '/chat/') {
+    $('#chat').addClass('active');
+  } else if (pathname === '/signup/') {
+    $('#signup').addClass('active');
+  } else if (window.location.toString().includes('/user/')){
+    $('#profile').addClass('active');
+  } 
+}
+
+// Like Button
+$('.like-btn').on('click', function (e) {
+  
+  e.preventDefault();
+  var element = $(this);
+  $.ajax({
+    url: '/like_img/',
+    method: 'GET',
+    data: {
+      media_id: element.attr('data-id')
+    },
+    success: function (response) {
+      element.html('Likes: ' + response)
+    }
+  })
+})
+
